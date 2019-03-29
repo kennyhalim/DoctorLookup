5ad8555b192e5c7ef4b80e511be468245b3f6573
Kenny Halim <kennyhalim98@gmail.com>
2019-03-29 08:16:01 -0700

    add initial files

---
+++ src/Doctor.js
@@ -0,0 +1,17 @@
+export class Doctor {
+  getDoctorByCondition(condition) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,24 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from 'jquery';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+
+
+    let weatherService = new WeatherService();  // create instance of WeatherService class
+    let promise = weatherService.getWeatherByCity(city);  // call the instance method and pass in user input
+
+    promise.then(function(response) {
+      body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
+});
